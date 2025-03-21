79e98a74b5444f0fb6b83bfc204654e05495c81a
Nguyễn Ngọc Cẩm Tiên <142108407+Camtien25@users.noreply.github.com>
2025-03-21 09:14:51 +0700

    Delete BAO CAO directory

--- BAO CAO/view/navngang.php
+++
@@ -1,9 +0,0 @@
-<a href="index.php">Trang chủ</a>
-<?php
-    if(isset($_SESSION["dn"])){
-        echo '<a href="?act=logout" onclick="return confirm(\'Bạn thật sự muốn thoát?\');">Đăng xuất';
-
-    } else {
-        echo '<a href="?act=regis">Đăng ký</a> | <a href="?act=login">Đăng nhập</a>';
-    }
-?>
